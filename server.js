2e5e59efe8632056a51ea00c5f9014ce820d6855
thanhle <thanhlcm90@gmail.com>
2014-05-06 17:51:34 +0700

    Add procfile heroku and change default mode

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // load configurations for each mode, default is development
-var env = process.env.NODE_ENV || 'development',
-  config = require('./config/config').init(env);
+var env = process.env.NODE_ENV || 'production',
+    config = require('./config/config').init(env);
 
 // dependencies
 var restify = require('restify'),
-  mongoose = require('mongoose/'),
-  fs = require('fs');
+    mongoose = require('mongoose/'),
+    fs = require('fs');
 
 // Paths global
 global.__models_path = config.root + '/models';
@@ -16,54 +16,54 @@ global.__routes_path = config.root + '/routes';
 var connectStr = config.db_prefix + '://' + config.host + ':' + config.db_port + '/' + config.db_database;
 console.log(connectStr);
 mongoose.connect(connectStr, {
-  server: {
-    auto_reconnect: true
-  }
+    server: {
+        auto_reconnect: true
+    }
 });
 var db = mongoose.connection;
 
 // log event for database 
 db.on('opening', function() {
-  console.log("reconnecting... %d", mongoose.connection.readyState);
+    console.log("reconnecting... %d", mongoose.connection.readyState);
 });
 db.once('open', function callback() {
-  console.log("Database connection opened.");
+    console.log("Database connection opened.");
 });
 db.on('error', function(err) {
-  console.log("DB Connection error %s", err);
+    console.log("DB Connection error %s", err);
 });
 db.on('reconnected', function() {
-  console.log('MongoDB reconnected!');
+    console.log('MongoDB reconnected!');
 });
 db.on('disconnected', function() {
-  console.log('MongoDB disconnected!');
-  mongoose.connect(connectStr, {
-    server: {
-      auto_reconnect: true
-    }
-  });
+    console.log('MongoDB disconnected!');
+    mongoose.connect(connectStr, {
+        server: {
+            auto_reconnect: true
+        }
+    });
 });
 
 // Bootstrap models
 fs.readdirSync(__models_path).forEach(function(file) {
-  // ignore .ds_store file in MAC OS
-  if (~file.indexOf('.js')) {
-    console.log("Loading model " + file);
-    require(__models_path + '/' + file);
-  }
+    // ignore .ds_store file in MAC OS
+    if (~file.indexOf('.js')) {
+        console.log("Loading model " + file);
+        require(__models_path + '/' + file);
+    }
 });
 
 // create server
 var app = restify.createServer({
-  name: 'restful'
+    name: 'restful'
 });
 app.on('error', function(err) {
-  if (err.errno === 'EADDRINUSE') {
-    console.log('Port already in use.');
-    process.exit(1);
-  } else {
-    console.log(err);
-  }
+    if (err.errno === 'EADDRINUSE') {
+        console.log('Port already in use.');
+        process.exit(1);
+    } else {
+        console.log(err);
+    }
 
 });
 
